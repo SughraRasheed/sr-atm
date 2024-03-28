ac4abb25dafb4f02e2e27e8f6d33b3edefbce4f5
SughraRasheed <159168716+SughraRasheed@users.noreply.github.com>
2024-03-29 00:26:19 +0500

    This is my ATM commit

---
+++ main.js
@@ -0,0 +1,40 @@
+import inquirer from "inquirer";
+let myBalance = 10000;
+let myPin = 1234;
+console.log("Welcome to SUGHRA RASHEED - ATM");
+let pinAnswer = await inquirer.prompt({
+    name: "pin",
+    type: "number",
+    message: "Enter your pin",
+});
+if (pinAnswer.pin === myPin) {
+    console.log("Correct pin code");
+}
+else {
+    console.log("Incorrect pin code, Try Again");
+    process.exit();
+}
+let operationAns = await inquirer.prompt([{
+        name: "operation",
+        type: "list",
+        message: "Select your operation",
+        choices: ["withdraw", "check balance"]
+    }]);
+if (operationAns.operation === "withdraw") {
+    let amountAnswer = await inquirer.prompt([{
+            name: "amount",
+            type: "number",
+            message: "Enter your amount to withdraw",
+        }]);
+    if (amountAnswer.amount > myBalance) {
+        console.log("Insufficient balance");
+    }
+    else {
+        myBalance -= amountAnswer.amount;
+        console.log("Your remaining new balance is " + myBalance);
+    }
+}
+;
+if (operationAns.operation === "check balance") {
+    console.log("Your current balance is " + myBalance);
+}
